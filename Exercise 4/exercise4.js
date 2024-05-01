a2bf8022344c833349fe0fcae749d28196406686
JarrodJackson2000 <jarrodjackson2000@hotmail.com>
2024-05-01 16:50:16 +1200

    Added Exercise 4

--- Exercise 4/exercise4.js
+++ Exercise 4/exercise4.js
@@ -1,1 +1,26 @@
-// 1. Create 4 functions for the 4 main mathematical operations (-,+,/,*). Return the calculated value and then output it to the screen.
+// 1. Create 4 functions for the 4 main mathematical operations (-,+,/,*).
+// Return the calculated value and then output it to the screen.
+
+function addition(a, b) {
+  return a + b;
+}
+
+function subtraction(a, b) {
+  return a - b;
+}
+
+function multiplication() {
+  return a * b;
+}
+
+function division() {
+  return a / b;
+}
+
+console.log(addition(5, 5));
+
+console.log(subtraction(5, 5));
+
+console.log(multiplication(5, 5));
+
+console.log(division(5, 5));
